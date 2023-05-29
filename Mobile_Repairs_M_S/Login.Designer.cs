f2abeda81ef955083a7107dfd6ad033054b0db31
Ahmedkhaledhassan <117466876+Ahmedkhaledhassan@users.noreply.github.com>
2023-05-29 14:31:04 -0700

    login buttons

--- Mobile_Repairs_M_S/Login.Designer.cs
+++ Mobile_Repairs_M_S/Login.Designer.cs
@@ -130,7 +130,7 @@ namespace Mobile_Repairs_M_S
             this.LoginBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.LoginBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.LoginBtn.ForeColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.LoginBtn.Location = new System.Drawing.Point(346, 269);
+            this.LoginBtn.Location = new System.Drawing.Point(348, 269);
             this.LoginBtn.Margin = new System.Windows.Forms.Padding(2);
             this.LoginBtn.Name = "LoginBtn";
             this.LoginBtn.Size = new System.Drawing.Size(139, 41);
