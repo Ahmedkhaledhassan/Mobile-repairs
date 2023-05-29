bc8f36648b8dcd1a6338b17150fc8ab7e28a3b4a
Ahmedkhaledhassan <117466876+Ahmedkhaledhassan@users.noreply.github.com>
2023-05-29 14:58:50 -0700

    Edit button in customers page

--- Mobile_Repairs_M_S/Customers.cs
+++ Mobile_Repairs_M_S/Customers.cs
@@ -83,7 +83,7 @@ namespace Mobile_Repairs_M_S
             {
                 if (CustNameTb.Text == "" || CustPhoneTb.Text == "" || CustAddTb.Text == "")
                 {
-                    MessageBox.Show("Missing Data");
+                    MessageBox.Show("Missing the Data");
                 }
                 else
                 {
