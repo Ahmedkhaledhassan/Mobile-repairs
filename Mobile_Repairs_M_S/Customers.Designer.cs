5ddbc39203722fb374936e8e9d3f4fccdf6e59f8
Ahmedkhaledhassan <117466876+Ahmedkhaledhassan@users.noreply.github.com>
2023-05-29 14:34:09 -0700

    mobile repairs pannel

--- Mobile_Repairs_M_S/Customers.Designer.cs
+++ Mobile_Repairs_M_S/Customers.Designer.cs
@@ -30,9 +30,9 @@ namespace Mobile_Repairs_M_S
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Customers));
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
@@ -70,7 +70,7 @@ namespace Mobile_Repairs_M_S
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(125, 16);
+            this.label1.Location = new System.Drawing.Point(126, 16);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(390, 26);
@@ -91,7 +91,7 @@ namespace Mobile_Repairs_M_S
             this.panel1.Controls.Add(this.RepairesPic);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(120, 609);
             this.panel1.TabIndex = 3;
@@ -100,7 +100,7 @@ namespace Mobile_Repairs_M_S
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(12, 11);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(77, 76);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -161,7 +161,7 @@ namespace Mobile_Repairs_M_S
             // 
             this.LogoutPic.Image = ((System.Drawing.Image)(resources.GetObject("LogoutPic.Image")));
             this.LogoutPic.Location = new System.Drawing.Point(24, 500);
-            this.LogoutPic.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.LogoutPic.Margin = new System.Windows.Forms.Padding(2);
             this.LogoutPic.Name = "LogoutPic";
             this.LogoutPic.Size = new System.Drawing.Size(76, 62);
             this.LogoutPic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -173,7 +173,7 @@ namespace Mobile_Repairs_M_S
             // 
             this.SparesPic.Image = ((System.Drawing.Image)(resources.GetObject("SparesPic.Image")));
             this.SparesPic.Location = new System.Drawing.Point(16, 255);
-            this.SparesPic.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.SparesPic.Margin = new System.Windows.Forms.Padding(2);
             this.SparesPic.Name = "SparesPic";
             this.SparesPic.Size = new System.Drawing.Size(73, 80);
             this.SparesPic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -185,7 +185,7 @@ namespace Mobile_Repairs_M_S
             // 
             this.CustomersPic.Image = ((System.Drawing.Image)(resources.GetObject("CustomersPic.Image")));
             this.CustomersPic.Location = new System.Drawing.Point(12, 130);
-            this.CustomersPic.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.CustomersPic.Margin = new System.Windows.Forms.Padding(2);
             this.CustomersPic.Name = "CustomersPic";
             this.CustomersPic.Size = new System.Drawing.Size(77, 76);
             this.CustomersPic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -197,7 +197,7 @@ namespace Mobile_Repairs_M_S
             // 
             this.RepairesPic.Image = ((System.Drawing.Image)(resources.GetObject("RepairesPic.Image")));
             this.RepairesPic.Location = new System.Drawing.Point(16, 375);
-            this.RepairesPic.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.RepairesPic.Margin = new System.Windows.Forms.Padding(2);
             this.RepairesPic.Name = "RepairesPic";
             this.RepairesPic.Size = new System.Drawing.Size(76, 89);
             this.RepairesPic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -212,7 +212,7 @@ namespace Mobile_Repairs_M_S
             this.SaveBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.SaveBtn.ForeColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.SaveBtn.Location = new System.Drawing.Point(136, 343);
-            this.SaveBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.SaveBtn.Margin = new System.Windows.Forms.Padding(2);
             this.SaveBtn.Name = "SaveBtn";
             this.SaveBtn.Size = new System.Drawing.Size(82, 41);
             this.SaveBtn.TabIndex = 11;
@@ -224,7 +224,7 @@ namespace Mobile_Repairs_M_S
             // 
             this.CustPhoneTb.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.CustPhoneTb.Location = new System.Drawing.Point(130, 178);
-            this.CustPhoneTb.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.CustPhoneTb.Margin = new System.Windows.Forms.Padding(2);
             this.CustPhoneTb.Name = "CustPhoneTb";
             this.CustPhoneTb.Size = new System.Drawing.Size(201, 28);
             this.CustPhoneTb.TabIndex = 10;
@@ -245,7 +245,7 @@ namespace Mobile_Repairs_M_S
             // 
             this.CustNameTb.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.CustNameTb.Location = new System.Drawing.Point(131, 107);
-            this.CustNameTb.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.CustNameTb.Margin = new System.Windows.Forms.Padding(2);
             this.CustNameTb.Name = "CustNameTb";
             this.CustNameTb.Size = new System.Drawing.Size(201, 28);
             this.CustNameTb.TabIndex = 8;
@@ -266,7 +266,7 @@ namespace Mobile_Repairs_M_S
             // 
             this.CustAddTb.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.CustAddTb.Location = new System.Drawing.Point(131, 228);
-            this.CustAddTb.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.CustAddTb.Margin = new System.Windows.Forms.Padding(2);
             this.CustAddTb.Multiline = true;
             this.CustAddTb.Name = "CustAddTb";
             this.CustAddTb.Size = new System.Drawing.Size(201, 101);
@@ -292,7 +292,7 @@ namespace Mobile_Repairs_M_S
             this.DeleteBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.DeleteBtn.ForeColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.DeleteBtn.Location = new System.Drawing.Point(183, 388);
-            this.DeleteBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.DeleteBtn.Margin = new System.Windows.Forms.Padding(2);
             this.DeleteBtn.Name = "DeleteBtn";
             this.DeleteBtn.Size = new System.Drawing.Size(97, 41);
             this.DeleteBtn.TabIndex = 14;
@@ -307,7 +307,7 @@ namespace Mobile_Repairs_M_S
             this.EditBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.EditBtn.ForeColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.EditBtn.Location = new System.Drawing.Point(228, 343);
-            this.EditBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.EditBtn.Margin = new System.Windows.Forms.Padding(2);
             this.EditBtn.Name = "EditBtn";
             this.EditBtn.Size = new System.Drawing.Size(86, 41);
             this.EditBtn.TabIndex = 15;
@@ -319,7 +319,7 @@ namespace Mobile_Repairs_M_S
             // 
             this.panel2.BackColor = System.Drawing.Color.Gray;
             this.panel2.Location = new System.Drawing.Point(128, 44);
-            this.panel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(960, 5);
             this.panel2.TabIndex = 16;
@@ -329,37 +329,37 @@ namespace Mobile_Repairs_M_S
             // 
             this.panel3.BackColor = System.Drawing.Color.Black;
             this.panel3.Location = new System.Drawing.Point(350, 98);
-            this.panel3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel3.Margin = new System.Windows.Forms.Padding(2);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(8, 600);
             this.panel3.TabIndex = 17;
             // 
             // CustomersList
             // 
-            dataGridViewCellStyle4.BackColor = System.Drawing.Color.White;
-            this.CustomersList.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle4;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.White;
+            this.CustomersList.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
             this.CustomersList.BackgroundColor = System.Drawing.Color.Gainsboro;
-            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
-            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle5.ForeColor = System.Drawing.Color.White;
-            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.CustomersList.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle5;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.CustomersList.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
             this.CustomersList.ColumnHeadersHeight = 24;
             this.CustomersList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
-            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle6.BackColor = System.Drawing.Color.White;
-            dataGridViewCellStyle6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
-            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
-            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
-            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.CustomersList.DefaultCellStyle = dataGridViewCellStyle6;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.CustomersList.DefaultCellStyle = dataGridViewCellStyle3;
             this.CustomersList.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
             this.CustomersList.Location = new System.Drawing.Point(382, 98);
-            this.CustomersList.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.CustomersList.Margin = new System.Windows.Forms.Padding(2);
             this.CustomersList.Name = "CustomersList";
             this.CustomersList.ReadOnly = true;
             this.CustomersList.RowHeadersVisible = false;
@@ -410,7 +410,7 @@ namespace Mobile_Repairs_M_S
             this.Controls.Add(this.label2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Customers";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Customers";
